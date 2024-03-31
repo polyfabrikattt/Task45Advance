b299171eba0ab651c8e54e0e45330ceefd474575
Артуриус <@DESKTOP>
2024-03-31 14:07:52 +0300

    Добавьте файлы проекта.

---
+++ enc_temp_folder/e8bfae97b18b70f7f92287f092ddd817/lesson76.cpp
@@ -0,0 +1,40 @@
+﻿#include <iostream>
+using namespace std;
+int main() {
+    const int kol = 5;
+    int a[kol], b[kol], c[kol + kol], i, j, k;
+    cout << "Input first massive(5 elements)";
+    for (i = 0; i < kol; i++)
+        cin >> a[i];
+    cout << "input second massive(5 elements)";
+    for (i = 0; i < kol; i++)
+        cin >> b[i];
+    i = 0, j = 0, k = 0;
+    do {
+        while (a[i] < b[j] && i < kol)
+        {
+            c[k++] = a[i];
+            i++;
+        }
+        while (a[i] > b[j] && j < kol)
+        {
+            c[k++] = b[j];
+            j++;
+        }
+        while (a[i] == b[i] && i < kol && j < kol)
+        {
+            c[k++] = a[i++];
+            c[k++] = b[j++];
+        }
+        if (i == kol)
+            while (j < kol)
+                c[k++] = b[j++];
+        if (j == kol)
+            while (i < kol)
+                c[k++] = a[i++];
+    } while (k < kol + kol);
+        for (i = 0; i < kol + kol; i++) {
+            cout << c[i] << ' ';
+        
+    }
+}
---
+++ lesson76/lesson76.cpp
@@ -0,0 +1,40 @@
+﻿#include <iostream>
+using namespace std;
+int main() {
+    const int kol = 5;
+    int a[kol], b[kol], c[kol + kol], i, ii, iii;
+    cout << "Input first massive(5 elements)";
+    for (i = 0; i < kol; i++)
+        cin >> a[i];
+    cout << "input second massive(5 elements)";
+    for (i = 0; i < kol; i++)
+        cin >> b[i];
+    i = 0, ii = 0, iii = 0;
+    do {
+        while (a[i] < b[ii] && i < kol)
+        {
+            c[iii++] = a[i];
+            i++;
+        }
+        while (a[i] > b[ii] && ii < kol)
+        {
+            c[iii++] = b[ii];
+            ii++;
+        }
+        while (a[i] == b[i] && i < kol && ii < kol)
+        {
+            c[iii++] = a[i++];
+            c[iii++] = b[ii++];
+        }
+        if (i == kol)
+            while (ii < kol)
+                c[iii++] = b[ii++];
+        if (ii == kol)
+            while (i < kol)
+                c[iii++] = a[i++];
+    } while (iii < kol + kol);
+        for (i = 0; i < kol + kol; i++) {
+            cout << c[i] << ' ';
+        
+    }
+}
